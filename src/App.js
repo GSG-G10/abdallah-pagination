f815f8ab5f361cfb1ab18057554cac4029b72e54
aaamra <dev.aaamra@gmail.com>
2021-09-28 15:07:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Card } from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
+
+const { Meta } = Card;
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(1);
+
+
+  const cardsList = cards.map((card) => 
+          <Card 
+            key={card.id}
+            hoverable
+            style={{width : '18%', "marginBottom" : '1rem'}}
+            cover={<img alt="example" 
+            src={card.preview_photos[0].urls.regular} />}
+          >
+            <Meta title={card.title} description={card.description}/>
+          </Card>   
+);
+
+  useEffect(() => {
+    fetch(`https://api.unsplash.com/search/collections?page=${page}&query=cat&client_id=TwvKJh4prHYcX8rjZdWBMDAHv0-h5ceFoB0Gg3irklI`)
+    .then(res => res.json())
+    .then(data => {
+      setCards(data.results);
+    });
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="cards">
+          {cardsList}
+      </div>
     </div>
   );
 }
